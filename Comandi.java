894c1a54912a398602142c641451a783ce7fef9d
Giacomix02 <giacomopaolocci@gmail.com>
2021-12-03 20:50:44 +0100

    stringe e metodi

--- Comandi.java
+++ Comandi.java
@@ -36,12 +36,46 @@ public class Comandi { // il nome della classe deve essere lo stesso del nome de
 
         // _______________________________________________________________________________________________________________________________________________
 
+        //ARRAY BIDIMENSIONALI
+
+        int [][] a = new int[3][];  //dichiaro il l'array monodimensionale
+
+        //Il numero di coppie di [] è pari alla dimensione dell'array
+
+        /*
+            in questo caso l'array sarà inizializzato come:
+
+            null
+            null
+            null
+
+        */
+
+        a[0] = new int[2];      //
+        a[1] = new int[4];      //dichiaro le righe dell'array
+        a[2] = new int[3];      //
+
+        //Le righe di un array bidim. possono avere lunghezze diverse
+
+
+        /*In fase di creazione di un array bidim., è possibile specificare un valore numerico anche dentro la seconda coppia di []:
+
+            int [][] a1 = new int [3][2];
+
+            In tal caso viene dichiarato un identificatore a1 per riferire un array bidimensionli di interi avente 3 righe, tutte di lunghezza pari a 2 (ovvero,una matrice 3x2).
+        */
+
+        int [][] b = {{1 ,2 ,3} ,{5 ,6 ,7 ,9} ,{ -2 , -5}}; //creazione per enumerazione
+
+
+        // _______________________________________________________________________________________________________________________________________________
+
         // STRINGHE
 
         String s = "parola";
         String s1 = "Ciao";
         String t;
-
+        
         // metodo lenght
         Intero3 = s.length(); // restituisce in Intero3 il numeoro di caratteri nella stringa
 
@@ -55,6 +89,14 @@ public class Comandi { // il nome della classe deve essere lo stesso del nome de
         // char, int ecc
 
         t = s + 't'; // non da errore a differenza di concat ache se i tipi sono diversi
+
+        //estrazione id caratteri
+        Carattere = s.charAt(i); //restituisce il carattere di s presente alla posizione i (i>=0)
+
+        //estrazione di sottostringhe
+        t = s.substring(i); //nuova stringa uguale alla sottostringa di s dalla posizione i fino alla fine di s
+
+        t = s.substring(i,j);   //nuova stringa uguale alla sottostringa di s dalla posizione i fino alla posizione j-1 inclusa
 
         // _______________________________________________________________________________________________________________________________________________
 
@@ -131,13 +173,17 @@ public class Comandi { // il nome della classe deve essere lo stesso del nome de
         for (int i; Intero3 < 10; i++) { // il ciclo continuerà ad iterare finchè la variabile Intero3 è minore di 10
 
             Intero3++;
-            // oppore altro codice
+            // oppure altro codice
 
         }
 
         // ***********
 
         // ciclo do-while
+
+        do{
+
+        }while();
 
         /*
          * 
@@ -213,5 +259,52 @@ public class Comandi { // il nome della classe deve essere lo stesso del nome de
                            // calcolo di espressioni vengono memorizzati in variabili implicite di tipo int
         byte b3 = (byte) (b1 + b2); // <== forma corretta per fare la somma, usando quidi il cast di variabili
 
+        // _______________________________________________________________________________________________________________________________________________
+
+        //I METODI
+
+        int funz1=7, funz2=5;
+
+        metodo1(funz1,funz2);   //chiamiamo il metodo passandogli i valori richiesti (in questo caso int)
+
+        Intero3=metodo1();  //chiamiamo il metodo SENZA passargli i valori richiesti ma inserendo il risultato del passato del return in Intero3
+
+
+        // _______________________________________________________________________________________________________________________________________________
+
+        //RICORSIONE
+        
+
+
     }
+
+
+
+    // _______________________________________________________________________________________________________________________________________________
+
+    // I METODI
+
+    // public class <nome> { ==> inserire se vogliamo creare un metodo in un altro
+    // file
+
+    public static int metodo1(int x, int y) { // ==> definizione del metodo all'interno dello stesso file
+        int Val = 0;
+
+        //altro codice
+
+        return Val; // restituisce un valore di tipo intero contenente nella variabile Val
+    }
+
+
+    /*
+        Se un metodo non restituisce alcun risultato, ovvero è un metodo nella cui intestazione compare la parola riservata void prima del
+        nome del metodo, allora la sintassi dell'istruzione di uscita è data solamente dalla parola riservata return seguita da ;.
+
+        La semantica dell'istruzione return; consiste nell'uscire dal metodo completando la sua esecuzione e nel restituire il controllo al metodo chiamante.
+
+        Se l'uscita da un metodo di tipo void coincide con la fine del corpo  del metodo, allora non è necessario mettere return;.
+    */
+
+    // _______________________________________________________________________________________________________________________________________________
+
 }
