16057959272a518f914a71462116a24403813e5a
Giacomix02 <paoloccigiacomo@gmail.com>
2021-12-11 21:29:49 +0100

    nuovo esercizio

--- Java/Esercitazioni secondo parziale/Prova Intermedia - 22 Novembre 2010/Es5/Esercizio5.java
+++ Java/Esercitazioni secondo parziale/Prova Intermedia - 22 Novembre 2010/Es5/Esercizio5.java
@@ -11,8 +11,14 @@ public class Esercizio5 {
         int k = 2;
         boolean out;
         
+        System.out.println("METODO ITERATIVO:");
         out = controllo(a,b,k);
         System.out.println(out);
+
+        System.out.println("\nMETODO RICORSIVO:");
+        out=controlloRicorsivo(a,b,k);
+        System.out.println(out);
+
     }
 
     public static boolean controllo(int[] a, int[] b, int k){
@@ -28,5 +34,20 @@ public class Esercizio5 {
         }
         
         return out;
+    }
+
+    public static boolean controlloRicorsivo(int[] a, int[] b, int k){
+        return controlloRicorsivo(a, b, k, 0, 0, true,0);
+    }
+
+    public static boolean controlloRicorsivo(int[] a, int[] b, int k,int i, int j, boolean out,int conta){
+        if(i>=a.length || !out) return out;
+        if(j==0) conta=0;
+        if(j>=b.length){
+            if(conta<k) out=false;
+            return controlloRicorsivo(a, b, k, i+1, 0, out, conta);
+        }
+        if(a[i]==b[j]) conta++;
+        return controlloRicorsivo(a, b, k, i, j+1, out, conta);
     }
 }
