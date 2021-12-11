a53cdadc7ceae8d91b7190f1b983098b83b51bf3
Giacomix02 <paoloccigiacomo@gmail.com>
2021-12-11 20:15:02 +0100

    nuovo esercizio

--- Java/Esercitazioni secondo parziale/Prova Intermedia - 22 Novembre 2010/Es5/Esercizio5.java
+++ Java/Esercitazioni secondo parziale/Prova Intermedia - 22 Novembre 2010/Es5/Esercizio5.java
@@ -6,5 +6,27 @@ public class Esercizio5 {
             volte in b, altrimenti il metodo restituisce false. Ad esempio, se a = {2,-3,5}, b =
             {5,-4,2,-3,2,-3,7,5,5} e k = 2, il metodo restituisce true.
         */
+        int[] a = {2,-3,5};
+        int[] b = {5,-4,2,-3,2,-3,7,5,5};
+        int k = 2;
+        boolean out;
+        
+        out = controllo(a,b,k);
+        System.out.println(out);
+    }
+
+    public static boolean controllo(int[] a, int[] b, int k){
+        boolean out=true;
+        int conta;
+
+        for(int i=0;i<a.length && out;i++){
+            conta=0;
+            for(int j=0;j<b.length;j++){
+                if(a[i]==b[j]) conta++;
+            }
+            if(conta<k) out=false;
+        }
+        
+        return out;
     }
 }
