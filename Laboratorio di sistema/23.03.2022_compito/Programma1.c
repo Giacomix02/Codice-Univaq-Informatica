5582651e2d2aa9ae28b6b5f2682dde90c7fc6441
giacomix02 <paoloccigiacomo@gmail.com>
2022-04-14 21:27:40 +0200

    inizio nuovi esercizi

---
+++ Laboratorio di sistema/23.03.2022_compito/Programma1.c
@@ -0,0 +1,33 @@
+/*
+A >> analisi statica del programma 
+      >> VIOLAZIONE o CONTSRAINT ==> descrizione regola violata
+            >> rimozione delle istruzioni contenenti violazioni
+    >> continuare l'analisi
+
+B >> analisi dinamica del programma
+      >> per ogni costrutto descrivere quello che succede quando viene eseguito
+
+      >> per ogni operatore che genera SIDE EFFECT:
+            >>  se genera un undefined behaviour
+                 ==> individuarlo e spiegare la causa
+                 ==> rimuovere l'istruzione
+                 ==> continuare l'analisi
+            >> se NON genera undefined behaviour
+                 >> descrivere come viene modificata la memoria 
+                        ==> variabile modificata e valore nuovo  (in caso di portable behavior)
+                        ==> quali sono i possibili nuovi valori (in caso di non-portable behavior)
+      
+      >> ulteriori undefined behaviour
+            ==> individuarli e spiegare da cosa vengono causati 
+            ==> rimuovere l'istruzione
+            ==> continuare l'analisi 
+*/
+
+
+int x = -1, y = 3, z = -5;
+int main(void)
+{
+    z = x++ * 3 - (x = 6);
+
+    return 0;
+}
