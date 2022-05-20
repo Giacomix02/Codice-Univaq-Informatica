7a2a340a7bbee56d40ac36e773fa97c739593b13
Giacomix02 <paoloccigiacomo@gmail.com>
2022-05-21 01:32:56 +0200

    Nuovi esercizi

---
+++ LPS/Esercizi/16.06.2022_classe/esercizio1.c
@@ -0,0 +1,7 @@
+struct str_type1{
+    long size;
+    unsigned short quantity;
+    float price;
+}v1_type1={111};
+
+struct str_type1 v2_type2 = {22,2,2.4f};
---
+++ LPS/Esercizi/16.06.2022_classe/esercizio2.c
@@ -0,0 +1,8 @@
+typedef struct{
+    struct str_type1 object;
+    char name[4];
+}str_type2;
+
+str_type2 v_type2 = {{120000,5,99.99},{'O','B','J','1'}};
+str_type2 a_type2[2];
+
